4f472d34e8ac549b2ea100364deaafae5fa6e7fa
C-TTech <65463598+C-TTech@users.noreply.github.com>
2020-08-11 20:05:56 +0100

    Create ex18.py

---
+++ ex18.py
@@ -0,0 +1,12 @@
+def print_two(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_one(arg99):
+    print(f"arg1: {arg99}")
+
+def print_none():
+    print("I got nothing. ")
+
+print_two("C", "T")
+print_one("Uno")
+print_none()
